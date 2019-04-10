f2dfed7e4ea2ae4200d154e9905652b101e01d34
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-10 19:46:38 -0400

    Solved bug during standup

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -28,19 +28,31 @@ describe("<Controls />", () => {
     expect(getByText(/close gate/i)).toBeEnabled();
   });
 
-  it("should display enabled 'lock gate' button after closing gate", () => {
+  it("should display enabled 'lock gate' button after closing gate with props", () => {
     const { getByText } = render(<Controls closed={true} />);
     // fireEvent.click(getByText(/close gate/i));
     expect(getByText(/lock gate/i)).toBeEnabled();
   });
 
-  it("should display disabled 'open gate' button after locking gate", () => {
+  it("should display disabled 'open gate' button after locking gate with props", () => {
     const { getByText } = render(<Controls closed={true} locked={true} />);
     expect(getByText(/open gate/i)).toBeDisabled();
   });
 
-  it("should display enabled 'unlock gate' button after locking gate", () => {
+  it("should display enabled 'unlock gate' button after locking gate with props", () => {
     const { getByText } = render(<Controls closed={true} locked={true} />);
     expect(getByText(/unlock gate/i)).toBeEnabled();
+  });
+
+  it("should display enabled 'open gate' and 'lock gate' buttons after unlocking gate with props", () => {
+    const { getByText } = render(<Controls closed={true} locked={false} />);
+    expect(getByText(/lock gate/i)).toBeEnabled();
+    expect(getByText(/open gate/i)).toBeEnabled();
+  });
+
+  it("should display enabled 'close gate' and disabled 'lock gate' buttons after opening gate with props", () => {
+    const { getByText } = render(<Controls closed={false} locked={false} />);
+    expect(getByText(/lock gate/i)).toBeDisabled();
+    expect(getByText(/close gate/i)).toBeEnabled();
   });
 })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,4 +11,11 @@ describe("<Dashboard />", () => {
   it("should render successfully", () => {
     render(<Dashboard />);
   });
+
+  // INTEGRATION TESTING?
+  // it("should display enabled 'lock gate' button after closing gate with props", () => {
+  //   const { getByText } = render(<Dashboard />);
+  //   fireEvent.click(getByText(/close gate/i));
+  //   expect(getByText(/lock gate/i)).toBeDisabled();
+  // });
 });
