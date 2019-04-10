92ef301ee74e798fc9e489f2fa40a508a5e8c170
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-10 17:12:25 -0400

    Created first test and added cleanup

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,14 @@
 // Test away!
+import React from 'react';
 import Display from './Display.js';
-
-import { render, fireEvent } from "react-testing-library";
+import { render, fireEvent, cleanup } from "react-testing-library";
 import "jest-dom/extend-expect";
 
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+
+describe("<Display />", () => {
+  it("renders successfully", () => {
+    render(<Display />);
+  });
+})
