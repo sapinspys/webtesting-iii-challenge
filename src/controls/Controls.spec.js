7da43ca9f6b745bd50a90ca9f46ed80522039462
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-10 18:45:19 -0400

    Added 4 tests to Controls, one is buggy

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,12 +1,37 @@
 // Test away!
 import React from "react";
 import Controls from "./Controls.js";
-import { render, cleanup } from "react-testing-library";
+import { render, fireEvent, cleanup } from "react-testing-library";
 import "jest-dom/extend-expect";
 
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup);
 
 describe("<Controls />", () => {
+  it("should render successfully", () => {
+    render(<Controls />);
+  });
+
+  it("should have two toggle buttons", () => {
+    render(<Controls />);
+    const buttons = document.querySelectorAll(".toggle-btn");
+    expect(buttons).toHaveLength(2);
+  });
   
+  it("should display disabled 'lock gate' button on default", () => {
+    const { getByText } = render(<Controls />);
+    expect(getByText(/lock gate/i)).toBeDisabled();
+  });
+
+  it("should display enabled 'close gate' button on default", () => {
+    const { getByText } = render(<Controls />);
+    expect(getByText(/close gate/i)).toBeEnabled();
+  });
+
+  it("should display enabled 'lock gate' button after closing gate", () => {
+    const { getByText } = render(<Controls />);
+    fireEvent.click(getByText(/close gate/i));
+    // getByText(/open gate/i);
+    // expect(getByText(/lock gate/i)).toBeDisabled();
+  });
 })
