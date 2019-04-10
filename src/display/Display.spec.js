ee95916350010928b4560d57881a3a0d79ffa702
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-10 17:28:40 -0400

    3 more Display tests created

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,6 @@
 // Test away!
-import React from 'react';
-import Display from './Display.js';
+import React from "react";
+import Display from "./Display.js";
 import { render, fireEvent, cleanup } from "react-testing-library";
 import "jest-dom/extend-expect";
 
@@ -11,4 +11,28 @@ describe("<Display />", () => {
   it("renders successfully", () => {
     render(<Display />);
   });
-})
+
+  it("should display unlocked on default", () => {
+    const { getByText } = render(<Display />);
+
+    getByText(/unlocked/i);
+  });
+
+  it("should display open on default", () => {
+    const { getByText } = render(<Display />);
+
+    getByText(/open/i);
+  });
+
+  it("should display closed if closed prop is true", () => {
+    const { getByText } = render(<Display closed={true} />);
+
+    getByText(/closed/i);
+  });
+
+  it("should display open if closed prop is false", () => {
+    const { getByText } = render(<Display closed={false} />);
+
+    getByText(/open/i);
+  });
+});
